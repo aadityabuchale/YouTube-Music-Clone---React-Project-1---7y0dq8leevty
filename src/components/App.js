f9e94508e169977ea3b2dc1c78dedea9703ed876
Aaditya Buchale <aadityabuchale15@gmail.com>
2023-08-30 00:06:12 +0530

    package updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-
-
-  return (
-    <div id="main">
-    </div>
-  )
-}
-
+    return (
+        <div id="main">
+            <h1>{"Hello World"}</h1>
+        </div>
+    );
+};
 
 export default App;
